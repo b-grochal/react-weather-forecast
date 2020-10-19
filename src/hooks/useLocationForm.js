b9046560f1a8686462bc423dbc47b2c72498b419
Bartosz <radu480@o2.pl>
2020-10-19 15:54:48 +0200

    Creted custom hook for location form.

---
+++ src/hooks/useLocationForm.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+const useLocationForm = (submitForm, validateForm) => {
+    const [values, setValues] = useState({
+        latitude: null,
+        longitude: null
+    });
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setErrors({});
+        setIsSubmitting(true);
+    }
+
+    useEffect(() => {
+        if (Object.keys(errors).length === 0 && isSubmitting) {
+            submitForm();
+        }
+    },
+        [errors]
+    );
+
+    return { handleChange, handleSubmit, values, errors};
+};
+
+export default useLocationForm;
