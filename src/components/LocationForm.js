fff0df8ecb418faca33f632c905486d8162e40ba
Bartosz <radu480@o2.pl>
2020-10-19 12:14:37 +0200

    Created empty files for location's form component.

---
+++ src/components/LocationForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LocationForm.css';
+
+const LocationForm = () => {
+
+
+
+    return (
+        <h2>Location form</h2>
+    )
+}
+
+export default LocationForm;
--- src/pages/LocationForecastPage.js
+++ src/pages/LocationForecastPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import LocationForm from '../components/LocationForm';
 
 const ForecastForLocationPage = () => {
     return (
-        <h1>Forecast for location page</h1>
+        <>
+            <h1>Forecast for location page</h1>
+            <LocationForm />
+        </>
     )
 };
 
