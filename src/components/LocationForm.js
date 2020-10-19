7587273587041679cb0b46b48fd641dbc6b16912
Bartosz <radu480@o2.pl>
2020-10-19 15:38:28 +0200

    Created template with basic styles for location form.

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -6,7 +6,36 @@ const LocationForm = () => {
 
 
     return (
-        <h2>Location form</h2>
+        <form className='form'>
+            <div className='form__inputs-container'>
+                <div className='form__input-group'>
+                    <label className='form__label'>Latitude</label>
+                    <input
+                        className='form__input'
+                        type='number'
+                        name='latitude'
+                        placeholder='50'
+                    />
+                </div>
+                <div className='form__input-group'>
+                    <label className='form__label'>Longitude</label>
+                    <input
+                        className='form__input'
+                        type='number'
+                        name='longitude'
+                        placeholder='50'
+                    />
+                </div>
+            </div>
+            <div className='form__buttons-container'>
+                <button className='form__button'>
+                    Get location
+                </button>
+                <button className='form__button'>
+                    Get forecast
+                </button>
+            </div>
+        </form>
     )
 }
 
--- src/pages/LocationForecastPage.js
+++ src/pages/LocationForecastPage.js
@@ -4,7 +4,6 @@ import LocationForm from '../components/LocationForm';
 const ForecastForLocationPage = () => {
     return (
         <>
-            <h1>Forecast for location page</h1>
             <LocationForm />
         </>
     )
