c5e83768c241d62189bb8ff3d9e14ebd3f4ebf60
Bartosz <radu480@o2.pl>
2020-10-24 20:59:14 +0200

    Created loading spinner. Implemented showing loading spinner during fetching weather forecast.

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -2,13 +2,14 @@ import React, { useContext } from 'react';
 import WeatherForecastItem from './WeatherForecastItem';
 import './WeatherForecast.css';
 import { WeatherForecastContext } from '../context/WeatherForecastContext';
+import LoadingSpinner from './LoadingSpinner';
 
 const WeatherForecast = () => {
     const { error, isLoading, weatherForecastList } = useContext(WeatherForecastContext);
 
     return (
         <div className='weather-forecast-container'>
-            { isLoading && <p className='weather-forecast-container__communicate-text'>Lodaing data</p>}
+            { isLoading && <LoadingSpinner />}
 
             {!isLoading && error ? (
                 <p className='weather-forecast-container__communicate-text'>Lodaing data</p>
